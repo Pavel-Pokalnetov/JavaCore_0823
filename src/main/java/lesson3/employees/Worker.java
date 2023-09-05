f4b4bdb449e1c1e8feb096f64663ab7cb9a9ee6b
RedX <redx@mail.ru>
2023-09-06 00:36:13 +0500

    lesson3+

---
+++ src/main/java/lesson3/employees/Worker.java
@@ -0,0 +1,29 @@
+package lesson3.employees;
+
+/**
+ * Класс Worker представляет сотрудника-рабочего, который является подклассом класса Employee.
+ */
+public class Worker extends Employee {
+
+    /**
+     * Конструктор для создания объекта Worker.
+     *
+     * @param name   Имя сотрудника
+     * @param age    Возраст сотрудника
+     * @param salary Зарплата сотрудника
+     */
+    public Worker(String name, int age, double salary) {
+        super(name, age, salary);
+        this.setType("Worker");
+    }
+
+    /**
+     * Метод для расчета зарплаты сотрудника-рабочего.
+     *
+     * @return Зарплата сотрудника-рабочего
+     */
+    @Override
+    public double calculateSalary() {
+        return this.getSalary();
+    }
+}
